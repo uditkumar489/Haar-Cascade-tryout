319f1f1c17310c6b904266bb506e764043d1878d
udit kumar <uditkumar489@gmail.com>
2018-01-01 18:49:30 +0530

    detection function added
    ..

--- detector.py
+++ detector.py
@@ -13,3 +13,15 @@ import cv2
 # Loading the cascades
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+
+# Defining a function that will do the detections
+def detect(gray, frame):
+    faces = face_cascade.detectMultiScale(gray, 2, 7)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = frame[y:y+h, x:x+w]
+        eyes = eye_cascade.detectMultiScale(roi_gray, 1, 5)
+        for (ex, ey, ew, eh) in eyes:
+            cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0, 255, 0), 2)
+    return frame
