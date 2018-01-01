85e2b2e74e4c1e6dcd15b43cd2b24cfa0826183e
udit kumar <uditkumar489@gmail.com>
2018-01-01 18:48:25 +0530

    cascades loaded to DETECTOR
    ..

--- detector.py
+++ detector.py
@@ -9,3 +9,7 @@ Created on Mon Jan 01 18:28:43 2018
 
 # Importing the libraries
 import cv2
+
+# Loading the cascades
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
