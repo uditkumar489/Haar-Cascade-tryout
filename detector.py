09cfdc1eb3d15c5c15c763cdc30d10c58b8a22fe
udit kumar <uditkumar489@gmail.com>
2018-01-02 06:15:54 +0530

    tuned multiScaleD
    tuned the parameters of detectMultiScale function for better results

--- detector.py
+++ detector.py
@@ -16,12 +16,12 @@ eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 
 # Defining a function that will do the detections
 def detect(gray, frame):
-    faces = face_cascade.detectMultiScale(gray, 2, 7)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
         roi_gray = gray[y:y+h, x:x+w]
         roi_color = frame[y:y+h, x:x+w]
-        eyes = eye_cascade.detectMultiScale(roi_gray, 1, 5)
+        eyes = eye_cascade.detectMultiScale(roi_gray, 1.1, 3)
         for (ex, ey, ew, eh) in eyes:
             cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0, 255, 0), 2)
     return frame
